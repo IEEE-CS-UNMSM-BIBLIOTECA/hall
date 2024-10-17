c8f13e273567c953e978360555af1e2eab43d65e
S4vi0r <gbp17@outlook.com>
2024-10-16 23:00:13 -0500

    feat(Landing): integrate BookSlider component into Landing page

--- src/pages/Landing/Landing.tsx
+++ src/pages/Landing/Landing.tsx
@@ -1,6 +1,7 @@
 import { Box, Button, Group, Stack, Text, Title } from '@mantine/core';
 import { useEffect, useState } from 'react';
 import PageShell from '@/layout/PageShell';
+import BookSlider from '@/components/BookSlider';
 
 const WORDS = ['CUANDO', 'COMO', 'DONDE', 'LO QUE'];
 
@@ -38,8 +39,8 @@ const Landing = () => {
               EMPIEZA HOY
             </Button>
           </Stack>
-          <Box flex={4} h="100%">
-
+          <Box flex={4} h="80%">
+            <BookSlider />
           </Box>
         </Group>
       </PageShell>
