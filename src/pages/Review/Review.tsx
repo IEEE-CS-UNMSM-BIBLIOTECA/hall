6f39e5cdd82502ad50275836b2b444427188b9c5
Stevan <stevanpersonal@gmail.com>
2024-11-08 01:43:35 -0500

    feat(/review/:id): add static layout

--- src/pages/Review/Review.tsx
+++ src/pages/Review/Review.tsx
@@ -1,7 +1,90 @@
+import { Rating, Title } from '@mantine/core';
+import LoremIpsum from 'react-lorem-ipsum';
+import { IconArrowLeft } from '@tabler/icons-react';
+import { useLocation } from 'wouter';
 import PageShell from '@/layout/PageShell';
+import { ReviewType } from '@/types';
+import UserBadge from '@/components/UserBadge';
+import LikeButton from '@/components/LikeButton';
+
+const data: ReviewType = {
+  id: 1,
+  title: 'Excelente',
+  content: 'Me encantó este libro, lo recomiendo mucho.',
+  rating: 5,
+  total_likes: 10,
+  liked: true,
+  author: {
+    id: 1,
+    name: 'John Doe',
+    profile_picture_url: 'https://placehold.co/50x50',
+  },
+  book: {
+    id: 0,
+    title: 'Título 1',
+    cover_url: 'https://via.placeholder.com/200x310',
+    author: {
+      id: 0,
+      name: 'Autor 1',
+    },
+  },
+};
 
 const Review = ({ id }: { id: string }) => {
-  return <PageShell>review/{id}</PageShell>;
+  const reviewQuery = { data };
+  const [, setLocation] = useLocation();
+
+  return (
+    <PageShell>
+      <div className="flex-center-page gap-xl">
+        <img
+          src={reviewQuery.data.book.cover_url}
+          alt={`Portada de ${reviewQuery.data.book.title}`}
+        />
+        <div className="stack gap-lg">
+          <section className="group gap-xs align-center">
+            <IconArrowLeft
+              className="icon-button"
+              onClick={() => setLocation(`/book/${reviewQuery.data.book.id}`)}
+            />
+            <div className="stack">
+              <a href={`/book/${reviewQuery.data.book.id}`}>
+                {reviewQuery.data.book.title}
+              </a>
+              <a className="fz-sm" href={`/author/${reviewQuery.data.book.author.id}`}>
+                {reviewQuery.data.book.author.name}
+              </a>
+            </div>
+          </section>
+          <main className="vertical-scroll py-xxl stack gap-lg">
+            <Rating
+              readOnly
+              value={reviewQuery.data.rating}
+            />
+            <Title order={3}>
+              {reviewQuery.data.title}
+            </Title>
+            <section>
+              {/* {reviewQuery.data.content} */}
+              <LoremIpsum p={4} />
+            </section>
+            <footer className="group space-between align-center">
+              <UserBadge
+                name={reviewQuery.data.author.name}
+                id={reviewQuery.data.author.id}
+              />
+              <LikeButton
+                totalLikes={data.total_likes}
+                liked={data.liked}
+                addLike={() => {}}
+                removeLike={() => {}}
+              />
+            </footer>
+          </main>
+        </div>
+      </div>
+    </PageShell>
+  );
 };
 
 export default Review;
